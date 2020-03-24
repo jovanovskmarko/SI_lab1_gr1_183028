92db9c2a2330f512b03f82bebd91ce1170f75b33
Marko Jovanovski <marko.jovanovski66@gmail.com>
2020-03-24 16:40:19 +0100

    student class finished

--- Student.java
+++ Student.java
@@ -2,16 +2,57 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
-	int []labPoints=new labPoints[100];
-	//TODO constructor
+	int []labPoints=new int[100];
 
-	//TODO seters & getters
+	public String getIndex() {
+		return index;
+	}
 
-	public double getAverage() {
-		//TODO
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public int[] getLabPoints() {
+		return labPoints;
+	}
+
+	public void setLabPoints(int[] labPoints) {
+		this.labPoints = labPoints;
+	}
+
+	public Student(String index, String firstName, String lastName) {
+		
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public double getAverage(int []niza) {
+		int zbir=0;
+		int counter=0;
+		double average;
+		for(int i=1;i<=niza.length;i++) {
+			zbir+=niza[i];
+			counter++;
+		}
+		average=zbir/counter;
+		return average;
+		
 	}
 
 	public boolean hasSignature() {
-		//TODO
+		if(labPoints.length>=8)
+			return true;
+		else
+			return false;
+						
 	}
 }
